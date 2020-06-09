859786b4304affb068e623666f8304139cf3881a
Mohamed <malbgal@gmail.com>
2020-06-08 19:26:37 -0700

    siteMetadata updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,27 @@
 import React from      "react"
+import { graphql } from 'gatsby'
 import Layout from "../components/Layout"
 import LandingPageContent from "../components/LandingPageContent"
 import hands from "../assets/img/hands.jpg"
 import "../utils/font-awesome"
 
+export const query = graphql`
+{
+  site{
+    siteMetadata{
+      title
+    }
+  }
+}
+`
 
-const caption = `The Yemeni American Resource Center is committed to providing a source of learning and growth for the entire
-Yemeni Community in the Bay Area`;
-const mainText = `Yemeni American Resource Center`
-
-export default () => {
+export default ({data}) => {
+  const siteTitle = data.site.siteMetadata.title;
+  const caption = `${siteTitle} is committed to providing a source of learning and growth for the entire
+                    Yemeni Community in the Bay Area`;
   return (
     <>
-      <Layout caption={caption} mainText={mainText} heroImage={hands} slantedImage={true}>
+      <Layout caption={caption} mainText={siteTitle} heroImage={hands} slantedImage={true}>
         <LandingPageContent />
       </Layout>
     </>
