4f7b7307bec937378100a163fd2d0229a5c7a4e8
Mohamed <malbgal@gmail.com>
2020-06-11 02:58:47 -0700

    fixed hero text responsiveness

--- src/components/homogenous/Hero.js
+++ src/components/homogenous/Hero.js
@@ -17,10 +17,10 @@ export default ({mainText, caption, heroImage, slanted}) => {
           <div className="items-center flex flex-wrap">
               <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
                   <div className="pr-0">
-                      <h1 className="text-white font-semibold text-3xl pt-10 lg:text-5xl">
+                      <h1 className="text-white font-semibold text-2xl pt-20 sm:text-5xl">
                           {mainText}
                       </h1>
-                  <p className="mt-4 text-lg sm:text-base text-gray-300">
+                  <p className="mt-4 sm:text-lg text-base text-gray-300">
                       {caption}
                   </p>
                   </div>
