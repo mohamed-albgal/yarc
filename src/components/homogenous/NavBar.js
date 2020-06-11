64da624e77d15c0bd0cd43b94f2890f8fe51d24e
Mohamed <malbgal@gmail.com>
2020-06-10 22:43:12 -0700

    fixed links vis on sm >> screens

--- src/components/homogenous/NavBar.js
+++ src/components/homogenous/NavBar.js
@@ -9,9 +9,9 @@ export default () => {
         e.preventDefault();
         setClosed(!closed);
     }
-    const openMenuBg = !closed ? {background: `url(${squares})`} : {};
+    const openMenuBg = !closed ? svgBG : {};
     return (
-        <nav className="  z-50 w-full absolute top-0 sm:flex sm:items-center sm:justify-between bg-transparent">
+        <nav className=" z-50 w-full absolute top-0 sm:flex sm:items-center sm:justify-between bg-transparent">
             <div className="py-3 pl-2 pr-6 flex justify-between  items-center">
                 {/*left*/}
                 <div className="sm:px-6 flex-shrink-0">
@@ -21,7 +21,7 @@ export default () => {
                 </div>
                 {/*right*/}
                 <div>
-                    <button onClick={e=> hamburgerClick(e)} type="button" className="sm:hidden block text-gray-400">
+                    <button onClick={e=> hamburgerClick(e)} type="button" className="sm:hidden block text-white">
                         <svg viewBox="0 0 100 80" className=" fill-current h-4 lg:h-6">
                             <rect x={70/!closed} width="180" height="9"></rect>
                             <rect x={40/!closed} y="30" width="100" height="9"></rect>
@@ -30,14 +30,12 @@ export default () => {
                     </button>
                 </div>
             </div>
-            <div style={openMenuBg} className={`${(!closed ? " h-64 pt-10 pb-8 -mt-2 transition-opacity opacity-100 duration-1000 ":" transition-opacity duration-1000 opacity-0 ")} 
-                            sm:px-20 sm:block pt-2 pb-4 sm:flex sm:p-8 sm:items-center sm:justify-between
-                            ` }>
-                <div class={closed ? "hidden": "visible"}>
-                    <NavElement  text="About" linkTo="/about" />
-                    <NavElement  text="Mission" linkTo="/mission"/>
-                    <NavElement  text="Blog" linkTo="/blog/first-blog"/>
-                </div>
+            <div style={openMenuBg} className={`${(!closed ? " h-64 pt-10 pb-8 -mt-2 transition-opacity duration-300 ":" sm:opacity-100  transition-opacity duration-300 opacity-0 ")} 
+            sm:px-20 sm:block pt-2 pb-4 sm:flex sm:p-8 sm:items-center sm:justify-between
+            ` }>
+                <NavElement  text="About" linkTo="/about" />
+                <NavElement  text="Mission" linkTo="/mission"/>
+                <NavElement  text="Blog" linkTo="/blog/first-blog"/>
             </div>
         </nav>
     )
@@ -50,4 +48,9 @@ const NavElement = ({linkTo, text}) => {
         </Link>
     )
 
+}
+
+const svgBG = {
+    backgroundColor: `#151515`,
+    backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='88' height='88' viewBox='0 0 200 200'%3E%3Cg %3E%3Cpolygon fill='%23111111' points='100 57.1 64 93.1 71.5 100.6 100 72.1'/%3E%3Cpolygon fill='%23141414' points='100 57.1 100 72.1 128.6 100.6 136.1 93.1'/%3E%3Cpolygon fill='%23111111' points='100 163.2 100 178.2 170.7 107.5 170.8 92.4'/%3E%3Cpolygon fill='%23141414' points='100 163.2 29.2 92.5 29.2 107.5 100 178.2'/%3E%3Cpath fill='%23181818' d='M100 21.8L29.2 92.5l70.7 70.7l70.7-70.7L100 21.8z M100 127.9L64.6 92.5L100 57.1l35.4 35.4L100 127.9z'/%3E%3Cpolygon fill='%23181818' points='0 157.1 0 172.1 28.6 200.6 36.1 193.1'/%3E%3Cpolygon fill='%231d1d1d' points='70.7 200 70.8 192.4 63.2 200'/%3E%3Cpolygon fill='%23222222' points='27.8 200 63.2 200 70.7 192.5 0 121.8 0 157.2 35.3 192.5'/%3E%3Cpolygon fill='%231d1d1d' points='200 157.1 164 193.1 171.5 200.6 200 172.1'/%3E%3Cpolygon fill='%23181818' points='136.7 200 129.2 192.5 129.2 200'/%3E%3Cpolygon fill='%23222222' points='172.1 200 164.6 192.5 200 157.1 200 157.2 200 121.8 200 121.8 129.2 192.5 136.7 200'/%3E%3Cpolygon fill='%23181818' points='129.2 0 129.2 7.5 200 78.2 200 63.2 136.7 0'/%3E%3Cpolygon fill='%23222222' points='200 27.8 200 27.9 172.1 0 136.7 0 200 63.2 200 63.2'/%3E%3Cpolygon fill='%231d1d1d' points='63.2 0 0 63.2 0 78.2 70.7 7.5 70.7 0'/%3E%3Cpolygon fill='%23222222' points='0 63.2 63.2 0 27.8 0 0 27.8'/%3E%3C/g%3E%3C/svg%3E")`,
 }
---
+++ src/pages/blog/blog4-how-to-use-markdown.md
@@ -0,0 +1,24 @@
+﻿# 4th Blog
+
+## Can create checkable lists
+	
+
+ - [ ] Thing 1
+ - [ ] ~~Thing 2~~
+ - [ ] Thing 3
+ - [ ] Thing 4
+ - [x] ~~thing 5 done~~
+
+> Use a 'greater than sign' followed by a single space to create BLOCKQUOTES like is
+
+### Can import images:
+
+![enter image description here](https://cdn.guidingtech.com/imager/assets/210962/Cool-Flash-Wallpapers-in-HD-and-4K-for-Desktop-and-Mobile-1_4d470f76dc99e18ad75087b1b8410ea9.png?1545040423)
+### Can embed videos:
+#### On youtube share button, choose "embed", copy and paste that code right in!
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/PWhpx2oymFs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+
+### Export this as .md file, and send to me
