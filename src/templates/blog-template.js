2f1ba236838904749d8c09306692e76b180cc350
Mohamed <malbgal@gmail.com>
2020-06-12 02:05:09 -0700

    fixed all blog responsive cards

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -10,7 +10,7 @@ export default ({ data }) => {
         <div>
             <Layout mainText={frontmatter.title} caption={frontmatter.title} heroImage={pageTurn}>
             <div className=" text-center relative z-10">
-              <div className=" text-left inline-block -mb-8 -mt-20 h-full max-w-full whitespace-pre-wrap sm:max-w-none sm:px-6 px-4 sm:w-3/4 shadow-xl bg-white rounded-lg markdown" dangerouslySetInnerHTML={{__html:html}} />
+              <div className=" text-left inline-block -mb-8 -mt-20 h-full max-w-full whitespace-pre-wrap sm:max-w-none sm:px-6 px-4 sm:w-11/12 lg:w-9/12 shadow-xl bg-white rounded-lg markdown" dangerouslySetInnerHTML={{__html:html}} />
             </div>
             </Layout>
         </div>
