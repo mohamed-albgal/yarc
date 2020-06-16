5a7be12bf2fa61cf5f12b42acecd58beeaad4e59
Mohamed <malbgal@gmail.com>
2020-06-16 01:23:28 -0700

    minor

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -8,7 +8,7 @@ export default ({ data }) => {
     const {frontmatter, html } = data.markdownRemark;
     return (
         <div>
-            <Layout transparentNavBar>
+            <Layout navWithHero>
             <Hero mainText={frontmatter.title} caption={frontmatter.title} heroImage={dock}/>
             <div className=" text-center relative z-10">
               <div className=" text-left inline-block -mb-8 -mt-20 h-full max-w-full whitespace-pre-wrap sm:max-w-none sm:px-6 px-4 sm:w-11/12 lg:w-9/12 shadow-xl bg-white rounded-lg markdown" dangerouslySetInnerHTML={{__html:html}} />
