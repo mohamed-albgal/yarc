93ec55c8dc9b463604cd4fe76acf5cd5cda74606
Mohamed <malbgal@gmail.com>
2020-06-17 00:50:04 -0700

    added minor things to dropdown fully good on desktop, not mobile

--- src/components/homogenous/Navbutton.js
+++ src/components/homogenous/Navbutton.js
@@ -7,8 +7,8 @@ const NavButton = ({name, subMenu}) => {
     const ArrowIcon = ({h, w}) => {
         return (
             <svg height={h} width={w} transform={dropped && "rotate(180)"}>
-                <line x1="0" y1="0" x2={w/2} y2={h} style={{stroke:'rgb(0,0,0)',strokeWidth:'1.2'}} />
-                <line x1={w/2} y1={h} x2={w} y2="0" style={{stroke:'rgb(0,0,0)',strokeWidth:'1.2'}}/>
+                <line x1="0" y1="0" x2={w/2} y2={h} style={{stroke:'white',strokeWidth:'1.2'}} />
+                <line x1={w/2} y1={h} x2={w} y2="0" style={{stroke:'white',strokeWidth:'1.2'}}/>
             </svg>
         )
     }
@@ -19,14 +19,14 @@ const NavButton = ({name, subMenu}) => {
     }
     return (
         <div className="">
-            <div onClick={() => setDropped(!dropped)} className=" cursor-pointer block flex relative justify-center align-center w-20 p-2 rounded-sm bg-red-800">{"name"} 
-                <div className="block pl-2 pt-4"><ArrowIcon h="8" w="12"/></div>
+            <div onClick={() => setDropped(!dropped)} className=" text-white sm:cursor-pointer block  relative w-20 p-2 bg-transparent">{"name"} 
+                <div className="inline-block pl-2 pt-4"><ArrowIcon h="8" w="12"/></div>
             </div>
             {/*creates an invisible overlay that, when clicked, will close the dropdown, takes up entire w and h*/}
-            <div onClick={() => setDropped(false)}className={dropped && "fixed top-0 left-0 w-screen h-screen bg-black opacity-25 transparent"} />
+            <div onClick={() => setDropped(false)}className={dropped && "fixed top-0 left-0 w-screen h-screen transparent"} />
             
             { dropped &&
-                <div className=" absolute cursor-pointer text-black w-32 mt-2 py-1 pl-3 bg-blue-400 bg-white rounded-b-lg divide-y divide-gray-900">
+                <div className=" absolute cursor-pointer text-gray-400 w-32 mt-2 py-1 pl-3 bg-black shadow-lg rounded-lg divide-y divide-gray-900">
                     <div  className="py-2" onClick={() => navClick("/about")}>
                         {"About Us"}
                     </div>
