44622f9da207c25edb1b37109d9e78428ff2e276
Mohamed <malbgal@gmail.com>
2020-06-17 02:34:46 -0700

    navbutton back to own componenet

--- src/components/homogenous/NavBar.js
+++ src/components/homogenous/NavBar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import { Link, navigate } from 'gatsby'
 import logo from "../../assets/logo/duo_logo.svg"
-//import NavButton from './Navbutton'
+import NavButton from './Navbutton'
 
 
 export default ({withHero, animate}) => {
@@ -23,41 +23,6 @@ export default ({withHero, animate}) => {
                 <line x1="0" y1="0" x2={w/2} y2={h} style={{stroke:'rgb(255,255,255)',strokeWidth:'2'}} />
                 <line x1={w/2} y1={h} x2={w} y2="0" style={{stroke:'rgb(255,255,255)',strokeWidth:'2'}}/>
             </svg>
-        )
-    }
-
-    const ArrowIconSub = ({h, w}) => {
-        return (
-            <svg height={h} width={w} transform={dropped && "rotate(180)"}>
-                <line x1="0" y1="0" x2={w/2} y2={h} style={{stroke:'white',strokeWidth:'1.2'}} />
-                <line x1={w/2} y1={h} x2={w} y2="0" style={{stroke:'white',strokeWidth:'1.2'}}/>
-            </svg>
-        )
-    }
-
-    const navClick = (toPage) => {
-        setDropped(false);
-        navigate(toPage);
-    }
-    
-    const NavButton = ({linkTo, text, subMenu}) => {
-        const linkSlug = linkTo || `/${text.replace(" ","").toLowerCase()}`;
-        return (
-            <div className="">
-                <div onClick={() => setDropped(!dropped)} className="`text-center block  relative w-24 p-2  sm:ml-4  py-2 sm:font-hairline font-extrabold 
-                text-xl text-white sm:cursor-pointer  bg-transparent m:hover:scale-105 sm:hover:font-semibold transform duration-75">
-                    {text} 
-                    { subMenu && <div className="inline-block pl-2 pt-4"><ArrowIconSub h="8" w="12"/></div>}
-                </div>
-                {/*creates an invisible overlay that, when clicked, will close the dropdown, takes up entire w and h*/}
-                <div onClick={() => setDropped(false)}className={dropped && "fixed top-0 left-0 w-screen h-screen transparent"} />
-                
-                { (dropped && subMenu) &&
-                    <div className=" absolute cursor-pointer text-gray-400 w-32 mt-2 py-1 pl-3 bg-black shadow-lg rounded-lg divide-y divide-gray-900">
-                        {subMenu.map((text) => (<div  className="py-2" onClick={() => navClick(linkSlug)}>{text}</div>))}
-                    </div>
-                }
-            </div>
         )
     }
     
--- src/components/homogenous/Navbutton.js
+++ src/components/homogenous/Navbutton.js
@@ -1,13 +1,39 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
+import { navigate } from 'gatsby'
 
+const NavButton = ({text, subMenu, linkTo }) => {
+    const [dropped, setDropped] = useState(false)
+    const linkSlug = linkTo || `/${text.replace(" ","").toLowerCase()}`;
+    const navClick = (toPage) => {
+        setDropped(false);
+        navigate(toPage);
+    }
+    const ArrowIconSub = ({h, w}) => {
+        return (
+            <svg height={h} width={w} transform={dropped && "rotate(180)"}>
+                <line x1="0" y1="0" x2={w/2} y2={h} style={{stroke:'white',strokeWidth:'1.2'}} />
+                <line x1={w/2} y1={h} x2={w} y2="0" style={{stroke:'white',strokeWidth:'1.2'}}/>
+            </svg>
+        )
+    }
+    return (
+        <div className="">
+            <div onClick={() => setDropped(!dropped)} className="`text-center block  relative w-24 p-2  sm:ml-4  py-2 sm:font-hairline font-extrabold 
+            text-xl text-white sm:cursor-pointer  bg-transparent m:hover:scale-105 sm:hover:font-semibold transform duration-75">
+                {text} 
+                { subMenu && <div className="inline-block pl-2 pt-4"><ArrowIconSub h="8" w="12"/></div>}
+            </div>
+            {/*creates an invisible overlay that, when clicked, will close the dropdown, takes up entire w and h*/}
+            <div onClick={() => setDropped(false)}className={dropped && "fixed top-0 left-0 w-screen h-screen transparent"} />
+            
+            { (dropped && subMenu) &&
+                <div className=" absolute cursor-pointer text-gray-400 w-32 mt-2 py-1 pl-3 bg-black shadow-lg rounded-lg divide-y divide-gray-900">
+                    {subMenu.map((text) => (<div  className="py-2" onClick={() => navClick(linkSlug)}>{text}</div>))}
+                </div>
+            }
+        </div>
+    )
+}
 
-// const NavButton = ({name, subMenu}) => {
-//     const [dropped, setDropped] = useState(false)
-//     //const sublinks = [{name: "page 1", to:"/about"}, {name: "page 2", to: "/mission"}]
-
-    
- 
-// }
-
-// export default NavButton;
+export default NavButton;
 
