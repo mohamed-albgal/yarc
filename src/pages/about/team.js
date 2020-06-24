6d1de27dad27c4cad48f0bff8a38c8440899c034
Mohamed <malbgal@gmail.com>
2020-06-24 09:19:01 -0700

    fixed flex image cards

--- src/pages/about/team.js
+++ src/pages/about/team.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
 import Layout from '../../components/Layout'
 import Hero from '../../components/homogenous/Hero'
 import heroImage from '../../images/open-space.jpg'
 import image1 from '../../images/mokhtar.jpg'
+import image2 from '../../images/default-profile.png'
+import image3 from '../../images/flyer1.jpg'
+import image4 from '../../images/headshot4.jpg'
+import image5 from '../../images/hs_writing_ws.jpg'
+
+
 const heroText= "The YAR Center Team"
 
 const card1 = {
@@ -13,12 +18,39 @@ const card1 = {
     body: "description of this person's history",
     cardBgColor: "bg-teal-400"
 }
+const card2 = {
+    image: image2,
+    fullName: "Person 2",
+    title: "Director",
+    body: "description of this person's history",
+    cardBgColor: "bg-teal-400"
+}
+const card3 = {
+    image: image3,
+    fullName: "Person ",
+    title: "Director",
+    body: "description of this person's history",
+    cardBgColor: "bg-teal-400"
+}
+const card4 = {
+    image: image4,
+    fullName: "Person ",
+    title: "Director",
+    body: "description of this person's history",
+    cardBgColor: "bg-teal-400"
+}
+const card5 = {
+    image: image5,
+    fullName: "Person ",
+    title: "Director",
+    body: "description of this person's history",
+    cardBgColor: "bg-teal-400"
+}
 
-
-const SlantCard = ({bgColor="bg-teal-400", head, subHead, body, img, svgTextColor}) => {
+const SlantCard = ({bgColor="bg-teal-400", head, subHead, body, img, svgTextColor, height="h-64"}) => {
     return (
         <div className="flex flex-col  rounded-lg shadow-xl">
-            <img src={img} className=" rounded-t-lg object-cover" alt="staff image"/>
+            <img src={img} className=" w-full h-64 rounded-t-lg object-cover" alt="staff image"/>
             <div className="relative">
                 <svg
                 preserveAspectRatio="none"
@@ -48,18 +80,21 @@ export default () => {
         <Layout navWithHero bgGradientColor="bg-gradient-blue-bottom">
             <Hero mainText={heroText} heroImage={heroImage} />
             <div className="flex flex-wrap mt-10 z-10 relative">
-                <div className="px-4 pb-4 sm:w-1/3 flex-shrink">
+                <div className="px-4 pb-4 sm:w-1/3 w-full">
                     <SlantCard body={card1.body} svgTextColor={"text-teal-400"}  head={card1.fullName} img={card1.image} subHead={card1.title} bgColor={"bg-teal-400"} />
                 </div>
-                <div className="px-4 pb-4 sm:w-1/3 flex-shrink">
-                    <SlantCard body={card1.body} svgTextColor={"text-teal-400"}  head={card1.fullName} img={card1.image} subHead={card1.title} bgColor={"bg-teal-400"} />
+                <div className="px-4 pb-4 sm:w-1/3 w-full">
+                    <SlantCard body={card2.body} svgTextColor={"text-teal-400"}  head={card2.fullName} img={card2.image} subHead={card2.title} bgColor={"bg-teal-400"} />
                 </div>
-                <div className="px-4 pb-4 sm:w-1/3 flex-shrink">
-                    <SlantCard body={card1.body} svgTextColor={"text-teal-400"}  head={card1.fullName} img={card1.image} subHead={card1.title} bgColor={"bg-teal-400"} />
+                <div className="px-4 pb-4 sm:w-1/3 w-full">
+                    <SlantCard body={card3.body} svgTextColor={"text-teal-400"}  head={card3.fullName} img={card3.image} subHead={card3.title} bgColor={"bg-teal-400"} />
                 </div>
-                
-                
-                
+                <div className="px-4 pb-4 sm:w-1/3 w-full">
+                    <SlantCard body={card4.body} svgTextColor={"text-teal-400"}  head={card4.fullName} img={card4.image} subHead={card4.title} bgColor={"bg-teal-400"} />
+                </div>
+                <div className="px-4 pb-4 sm:w-1/3 w-full">
+                    <SlantCard body={card5.body} svgTextColor={"text-teal-400"}  head={card5.fullName} img={card5.image} subHead={card5.title} bgColor={"bg-teal-400"} />
+                </div>
             </div>
         </Layout>
     )
