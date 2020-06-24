e59e05df72532b2f62873124c299c1bb352d933b
Mohamed <malbgal@gmail.com>
2020-06-24 10:51:25 -0700

    fixed card animation

---
+++ src/components/homogenous/SlantCard.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const SlantCard = ({bgColor="bg-teal-400", head, subHead, body, img, svgTextColor, height="h-64"}) => {
+    return (
+        <div className="flex flex-col rounded-lg shadow-2xl">
+            <img src={img} className={` sm:max-w-full w-full ${height} rounded-t-lg object-cover object-top overflow-hidden" alt="staff image`}/>
+            <div className="relative">
+                <svg
+                preserveAspectRatio="none"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 583 95"
+                className={`absolute left-0 w-full block ${svgTextColor} fill-current`}
+                style={{
+                height: "95px",
+                top: "-94px"
+                }}
+                >
+                    <path
+                    d="M-30 95h613V65z"
+                    ></path>
+                </svg>
+            </div>
+            <div className={`${bgColor} text-center p-4 w-full rounded-b-lg leading-10`}>
+                <div id="name" className="-mt-3 sm:text-4xl text-3xl">{head}</div>
+                <div id="title" className="text-base text-gray-800 pb-2">{subHead}</div>
+                <div id="body" className="text-black sm:text-lg leading-6 leading-tight">{body}</div>
+            </div>
+        </div>
+    )
+}
+
+export default SlantCard
--- src/pages/about/team.js
+++ src/pages/about/team.js
@@ -7,6 +7,7 @@ import image2 from '../../images/default-profile.png'
 import image3 from '../../images/flyer1.jpg'
 import image4 from '../../images/headshot4.jpg'
 import image5 from '../../images/hs_writing_ws.jpg'
+import SlantCard from '../../components/homogenous/SlantCard'
 
 
 const heroText= "The YAR Center Team"
@@ -47,52 +48,24 @@ const card5 = {
     cardBgColor: "bg-teal-400"
 }
 
-const SlantCard = ({bgColor="bg-teal-400", head, subHead, body, img, svgTextColor, height="h-64"}) => {
-    return (
-        <div className="flex flex-col  rounded-lg shadow-xl">
-            <img src={img} className=" w-full h-64 rounded-t-lg object-cover" alt="staff image"/>
-            <div className="relative">
-                <svg
-                preserveAspectRatio="none"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 583 95"
-                className={`absolute left-0 w-full block ${svgTextColor} fill-current`}
-                style={{
-                height: "95px",
-                top: "-94px"
-                }}
-                >
-                    <path
-                    d="M-30 95h613V65z"
-                    ></path>
-                </svg>
-            </div>
-            <div className={`${bgColor} text-center p-4 w-full rounded-b-lg leading-10`}>
-                <div id="name" className="-mt-3 sm:text-4xl text-3xl">{head}</div>
-                <div id="title" className="text-base text-gray-800 pb-2">{subHead}</div>
-                <div id="body" className="text-black sm:text-lg leading-6 leading-tight">{body}</div>
-            </div>
-        </div>
-    )
-}
 export default () => {
     return (
         <Layout navWithHero bgGradientColor="bg-gradient-blue-bottom">
             <Hero mainText={heroText} heroImage={heroImage} />
             <div className="flex flex-wrap mt-10 z-10 relative">
-                <div className="px-4 pb-4 sm:w-1/3 w-full">
+                <div className="hover:scale-105 transform transition-transform duration-200 px-4 pb-4 sm:w-1/3 w-full h-full">
                     <SlantCard body={card1.body} svgTextColor={"text-teal-400"}  head={card1.fullName} img={card1.image} subHead={card1.title} bgColor={"bg-teal-400"} />
                 </div>
-                <div className="px-4 pb-4 sm:w-1/3 w-full">
+                <div className="hover:scale-105 transform transition-transform duration-200 px-4 pb-4 sm:w-1/3 w-full">
                     <SlantCard body={card2.body} svgTextColor={"text-teal-400"}  head={card2.fullName} img={card2.image} subHead={card2.title} bgColor={"bg-teal-400"} />
                 </div>
-                <div className="px-4 pb-4 sm:w-1/3 w-full">
+                <div className="hover:scale-105 transform transition-transform duration-200 px-4 pb-4 sm:w-1/3 w-full">
                     <SlantCard body={card3.body} svgTextColor={"text-teal-400"}  head={card3.fullName} img={card3.image} subHead={card3.title} bgColor={"bg-teal-400"} />
                 </div>
-                <div className="px-4 pb-4 sm:w-1/3 w-full">
+                <div className="hover:scale-105 transform transition-transform duration-200 px-4 pb-4 sm:w-1/3 w-full">
                     <SlantCard body={card4.body} svgTextColor={"text-teal-400"}  head={card4.fullName} img={card4.image} subHead={card4.title} bgColor={"bg-teal-400"} />
                 </div>
-                <div className="px-4 pb-4 sm:w-1/3 w-full">
+                <div className="hover:scale-105 transform transition-transform duration-200 px-4 pb-4 sm:w-1/3 w-full">
                     <SlantCard body={card5.body} svgTextColor={"text-teal-400"}  head={card5.fullName} img={card5.image} subHead={card5.title} bgColor={"bg-teal-400"} />
                 </div>
             </div>
