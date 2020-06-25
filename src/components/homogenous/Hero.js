d15f0a8c4b7ee7c1ae3333ae46a00c619292af46
Mohamed <malbgal@gmail.com>
2020-06-24 22:51:17 -0700

    minor things fixed

--- src/components/homogenous/Hero.js
+++ src/components/homogenous/Hero.js
@@ -19,24 +19,25 @@ export default ({mainText, caption, heroImage}) => {
           minHeight: "75vh"
       }}>
       {/* hero image*/}
-      <div className=" mobileHero md:shadow-2xl absolute inset-0 w-full h-full bg-fixed bg-cover"
+      <div className=" md:shadow-2xl absolute inset-0 w-full h-full sm:bg-fixed bg-cover bg-scroll"
           style={{
           backgroundImage: `url(${heroImage})`,
           WebkitOverflowScrolling: "touch",
           WebkitBackgroundSize: 'cover',
           MozBackgroundSize:"cover",
           }}>
-            <span id="blackOverlay" className="w-full h-full absolute opacity-0 bg-gray-300"></span> 
+            <span id="blackOverlay" className="w-full h-full absolute opacity-50 bg-indigo-300"></span> 
       </div>
       <div className="container relative mx-auto">
           <div className="items-center flex flex-wrap">
               <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
                   <div className="pr-0">
-                      <h1 className=" text-black leading-tight font-extrabold text-5xl pt-20 sm:text-7xl">
+                      <h1 className=" text-black leading-none  font-extrabold text-5xl pt-20 sm:text-6xl">
                           {mainText}
+                          
                       </h1>
                       <span className="title-gradient block h-1 -mt-2 self-stretch"></span>
-                  <p className="mt-4 text-lg sm:text-base font-bold text-gray-900">
+                  <p className="mt-4 text-lg sm:text-2xl font-bold text-gray-900">
                       {caption}
                   </p>
                   </div>
--- src/pages/events.js
+++ src/pages/events.js
@@ -12,15 +12,15 @@ import SlantCard from '../components/homogenous/SlantCard';
 
 export default  ({data}) => {
     
+    /**come back to this idea, random color for cards, but not random on each visit */
     //"text-teal-400", "text-orange-200", "btext-reen-500", "text-purple-200", "text-red-800", "text-blue-800", "text-indigo-700"
     //"bg-teal-400", "bg-orange-200", "bg-green-500", "bg-purple-200", "bg-red-800", "bg-blue-800", "bg-indigo-700"
-    const colorList = ["teal-400", "orange-200", "green-500", "purple-200", "red-800", "blue-800", "indigo-700"];
-    const randomColor = () => colorList[Math.floor(Math.random() * colorList.length)];
+    //const colorList = ["teal-400", "orange-200", "green-500", "purple-200", "red-800", "blue-800", "indigo-700"];
+    //const randomColor = () => colorList[Math.floor(Math.random() * colorList.length)];
     
     
     //get the array of nodes that contain the fields i need including the slug
-    const nodes = data.allMarkdownRemark.edges //.node.frontmatter
-    console.log(nodes);
+    const nodes = data.allMarkdownRemark.edges
     
     const dataBurger = nodes.map( ( { node } ) => (
             <div className="hover:scale-105 transform transition-transform duration-200 px-4 sm:pb-10 pb-4 sm:w-1/3 w-full h-full">
