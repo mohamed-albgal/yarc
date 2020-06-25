a67762f281523553a9feb09d3ebf1b492b45710a
Mohamed <malbgal@gmail.com>
2020-06-24 18:00:26 -0700

    fixed footer stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,7 +26,7 @@ const Footer = () => {
                       {/* address */}
                       <a href="https://maps.google.com/?q=8001 Capwell Dr Oakland CA 94621">
                         <FaBuilding className=" inline-block text-gray-600 text-2xl" />
-                        <div className="inline-block text-xs text-gray-500 tracking-widest">Address</div>
+                        <div className="inline-block pl-2 text-xs text-gray-500 tracking-widest">Address</div>
                         <div className="inline-block pl-4 text-sm sm:text-base tracking-wide text-gray-700"> 8001 Capwell Dr Oakland CA 94621</div>
                       </a>
                     </div>
@@ -35,7 +35,7 @@ const Footer = () => {
                       
                       <a href="tel:5102004175">
                         <FcPhone className=" inline-block text-2xl" />
-                        <div className="inline-block text-xs text-gray-500 tracking-widest">Phone</div>
+                        <div className="inline-block text-xs pl-2 text-gray-500 tracking-widest">Phone</div>
                         <div className="inline-block pl-6 ml-1 text-sm sm:text-base tracking-wider text-gray-700">510-200-4175</div>
                       </a>
                       
@@ -45,8 +45,8 @@ const Footer = () => {
                       
                       <a href="mailto:yayouthcenter@gmail.com">
                         <FcAddressBook className=" inline-block text-2xl" />
-                        <div className="inline-block text-xs text-gray-500 tracking-widest">Email</div>
-                        <div className="inline-block pl-8 text-sm sm:text-base tracking-wider text-gray-700">info@yarc.org</div>
+                        <div className="inline-block text-xs text-gray-500 pl-2 tracking-widest">Email</div>
+                        <div className="inline-block pl-8 text-sm sm:text-base tracking-wider text-gray-700">yayouthcenter@gmail.com</div>
                       </a>
                       
                     </div>
@@ -63,19 +63,20 @@ const Footer = () => {
                   Find us on any of these platforms, we'd love to connect!
                 </h5>
                 <div className="ml-0 flex flex-no-wrap">
-                  <button
+                  {/* <button
                     className="bg-transparent sm:text-4xl text-2xl my-2 mr-3 ml-0" style={{color:'#1DA1F2'}}
                     type="button"
                   >
-                    <FaTwitter />
-                  </button>
+                  <FaTwitter />
+                  </button> */}
                   <button
                     className="bg-transparent sm:text-4xl text-2xl my-2 mx-3" style={{color:'#3B5998'}}
                     type="button"
                   >
-                    <FaFacebook />
+                    <a href="https://www.facebook.com/Yemeni-American-Youth-Center-%D9%85%D8%B1%D9%83%D8%B2-%D8%A7%D9%84%D8%B4%D8%A8%D8%A7%D8%A8-%D8%A7%D9%84%D9%8A%D9%85%D9%86%D9%8A-%D8%A7%D9%84%D8%A3%D9%85%D8%B1%D9%8A%D9%83%D9%8A-110243897202862/">
+                    <FaFacebook /></a>
                   </button>
-                  <button
+                  {/* <button
                   className="bg-transparent sm:text-4xl text-2xl my-2 mx-3"
                     
                     type="button"
@@ -85,25 +86,25 @@ const Footer = () => {
                       color: 'white',
                       background: 'radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)',
                     }} />
-                  </button>
-                  <button
+                  </button>*/}
+                  {/*<button
                     className="bg-transparent sm:text-4xl text-2xl my-2 mx-3" style={{color:'#FF0000'}}
                     type="button"
                   >
                     <FaYoutube />
-                  </button>
+                  </button> */}
                   <button
                     className="bg-transparent sm:text-4xl text-3xl my-2 mx-3" style={{color:'#4ac959'}}
                     type="button"
                   >
-                    <FaWhatsapp />
+                    <a href="https://api.whatsapp.com/send?phone=15102004175"><FaWhatsapp /></a>
                   </button>
-                  <button
+                  {/*<button
                     className="bg-transparent sm:text-4xl text-2xl my-2 mx-3" style={{color:'#0072b1'}}
                     type="button"
                   >
                     <FaLinkedinIn />
-                  </button>
+                  </button>*/}
                 </div>
               </div>
             </div>
