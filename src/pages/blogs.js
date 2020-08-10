9cef12c6e0250127c0b4ef395ea146c037093365
Mohamed <malbgal@gmail.com>
2020-08-09 21:33:16 -0700

    fixed blog styling

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -62,8 +62,8 @@ export default ({data}) => {
             <div className=" sm:mt-0 sm:pl-16 sm:pt-10 mb-32 pt-32  " >
                 <PageHeadText text={intl.formatMessage({id:"blogHeadText"})} />
             </div>
-            <div className="mx-auto pb-10">
-                <div className="sm:flex justify-center mx-4">
+            <div className="lg:mx-20 mx-auto pb-10">
+                <div className="sm:flex align-middle flex-wrap lg:justify-start justify-center mx-4">
                     {allBlogs}
                 </div>
             </div>
@@ -75,17 +75,17 @@ export default ({data}) => {
 const BasicCard = ({title, excerpt, fluidImage=null, date, smallText, medText, height="h-64"}) => {
     const imageStyle = ` sm:max-w-full w-full ${height} rounded-t-lg object-cover object-top overflow-hidden" alt="staff image`
     return (
-        <div className="lg:max-w-md  rounded-md bg-gray-300 overflow-hidden shadow-2xl sm:mx-4 sm:mt-8 mt-4 ">
+        <div className="sm:hover:bg-white font-hairline max-w-sm sm:hover:text-blue-700  rounded-md bg-gray-200 overflow-hidden shadow-2xl sm:mx-6 sm:mt-8 my-20 ">
             <div className="">
                 {fluidImage ? <Img fluid={fluidImage} alt="something" className={imageStyle} />
                 : <img src={defaultImage} alt="blog bg Image" className={imageStyle} />
                 }
             </div>
-            <div className="px-6 py-4 w-full">
-                <div className="font-hairline bold sm:text-5xl text-4xl mb-2 sm:hover:text-indigo-800 "> {title}
+            <div className="px-6 py-4 w-full sm:hover:font-medium">
+                <div className=" sm:text-3xl min-w-full text-4xl mb-2 "> {title}
                 </div>
                 <p className="text-xs text-gray-500 leading-tight">{date}</p>
-                <p className="text-gray-700 text-2xl">
+                <p className="text-gray-700 text-md">
                 {excerpt}
                 </p>
                 <p className="text-gray-800 text-base tracking-wider">{medText}</p>
